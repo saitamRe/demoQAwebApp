8ebc258ff61b9d5553a5deb146adbc0b2478029e
Roman <fediuninrom5991@gmail.com>
2025-04-20 12:22:43 +0200

    init

---
+++ src/main/java/demoJavaProjects/App.java
@@ -0,0 +1,13 @@
+package demoJavaProjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
