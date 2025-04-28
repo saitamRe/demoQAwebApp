9b0771e799aacab307d851f82f4c3850ed7fbddd
Roman <fediuninrom5991@gmail.com>
2025-04-28 08:21:23 +0200

    superSmallFixFOrjenkins

---
+++ src/test/DemoTest.java
@@ -0,0 +1,19 @@
+import demoQATests.TestComponents.BaseTest;
+import org.testng.annotations.Test;
+
+
+public class DemoTest extends BaseTest {
+
+    @Test
+    public void someTest()
+    {
+        //lp.login();
+        System.out.println("HEY");
+    }
+
+
+
+
+
+
+}
