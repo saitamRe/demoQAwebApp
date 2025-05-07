92330f9bcd5b861ac7cb03d4ef729851947a963c
Roman <Roman@LAPTOP-ERNI922M>
2025-05-07 11:34:41 +0200

    screenShotMakerDraft

---
+++ src/test/java/data/DataReader.java
@@ -0,0 +1,14 @@
+package data;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class DataReader {
+    public String getDataFromJson() throws IOException {
+        File file = new File(System.getProperty("user.dir"+"src/test/java/data/PurchaseOrder.json"));
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+    }
+}
--- src/test/java/demoQATests/Tests/DemoTest.java
+++ src/test/java/demoQATests/Tests/DemoTest.java
@@ -1,21 +1,42 @@
 package demoQATests.Tests;
 
 import demoQATests.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
 
 
 public class DemoTest extends BaseTest {
 
-    @Test
-    public void someTest()
-    {
+    @Test(dataProvider = "getData")
+    public void someTest() {
         //lp.login();
         System.out.println("HEY");
     }
 
+    public void takeScreenshot(String tcName) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File input = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir")+"//Reports"+tcName+"//.png");
+        FileUtils.copyFile(input, file);
+    }
 
+    @DataProvider
+    public Object[][] getData()
+    {
+        HashMap<String, String> map1 = new HashMap<>();
+        map1.put("email","r@.com");
 
-
-
+        HashMap<String, String> map2 = new HashMap<>();
+        map2.put("email","f@.com");
+        return new Object[][]{{map1},{map2}};
+    }
 
 }
